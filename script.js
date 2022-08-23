5f59f00a750ac7951fa5f6e83fc3b10cb11c34d3
Oleh Pysmenko <olehpysmenko@blessing.local>
2022-08-23 10:57:07 +0100

    feat: image resolution

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function removeImages (){
 function loadImg() {
     removeImages();
 
-    const url = 'https://api.unsplash.com/search/photos/?query='+input.value+'&per_page=6&orientation=landscape&client_id=XYsZU4vahhHNhMEs3NQJttXa90B3Tkl-rc1Y7hq806Q'
+    const url = 'https://api.unsplash.com/search/photos/?query='+input.value+'&per_page=30&orientation=landscape&client_id=XYsZU4vahhHNhMEs3NQJttXa90B3Tkl-rc1Y7hq806Q'
 
     fetch(url)
 
@@ -29,7 +29,7 @@ function loadImg() {
         for(let i=0; i<data.results.length; i++){
             imageNodes[i]=document.createElement('div');
             imageNodes[i].className="img";
-            imageNodes[i].style.backgroundImage='url('+data.results[i].urls.raw + ')';
+            imageNodes[i].style.backgroundImage="url("+data.results[i].urls.raw + "&w=1366&h=768" +")";
             grid.appendChild(imageNodes[i]);
         }
     })
